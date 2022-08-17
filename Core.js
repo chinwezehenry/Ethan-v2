c2f9a5efab32b0e730fdf1f4ad52b58a2666182e
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-08-17 01:42:33 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -2571,7 +2571,7 @@ if (isBanChat) return reply(mess.bangc)
      if (!isBotAdmins) return replay(mess.botadmin)
      let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
      if (!users.length)return replay(`please write the number of the person you want to add`)
-      await Miku.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => replay(`✅Successfully Added!`)).catch((err) => replay(jsonformat(err)))
+      await Miku.groupParticipantsUpdate(m.chat, [users], 'add').then((res) => replay(`✅Successfully Added!`)).catch((err) => replay(`Unable to add user for some reason 😾`))
      }
      break
 
