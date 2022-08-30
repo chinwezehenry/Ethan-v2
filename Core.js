9788f9b425b9aba0fbd6c271b37d390920feee96
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-08-30 01:44:40 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1612,7 +1612,7 @@ break
 case 'capacity':  case 'bankupgrade': {
 	if (!isCreator) return replay(mess.botowner)
 	if (!text) return replay("💴 *Bank-capacity* 💳\n\n1 | *1000* = ¥100\n\n2 | *100000* = ¥1000\n\n3 | *10000000* = ¥10000000\n\nEx- /inc 1 OR /inc 1000")	
-	let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net' : m.sender
+	let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
 	const cara = "cara"
 	let value = text.trim();
 	let k = parseInt(value)
